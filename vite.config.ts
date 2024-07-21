1c97560ddc04bbfb838876834f3c1e1ba032d132
Mario Moreno <marioivanmorenopineda@gmail.com>
2024-07-21 12:51:47 -0400

    Modificacion de vite.config.ts y vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,44 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/",
-  plugins: [react()],
-  build: {
-    outDir: "./dist",
-    minify: 'terser', // Minifica el código JS/CSS
-    terserOptions: {
-      compress: {
-        drop_console: true, // Elimina las declaraciones console
-        drop_debugger: true, // Elimina las declaraciones debugger
+export default defineConfig(({ mode }) => {
+  // Cargar variables de entorno según el modo (desarrollo o producción)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: "/",
+    plugins: [react()],
+    build: {
+      outDir: "./dist",
+      minify: 'terser', 
+      terserOptions: {
+        compress: {
+          drop_console: true, 
+          drop_debugger: true,
+        },
+      },
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
+        },
       },
     },
-    rollupOptions: {
-      output: {
-        manualChunks: undefined, // Evita agrupar manualmente los chunks
+    server: {
+      proxy: {
+        '/api': 'http://localhost:3000', 
       },
     },
-  },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000', // Ejemplo de proxy para API local
+    resolve: {
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
     },
-  },
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
-  }
+    define: {
+      'import.meta.env.VITE_EMAILJS_USER_ID': JSON.stringify(env.VITE_EMAILJS_USER_ID),
+      'import.meta.env.VITE_EMAILJS_SERVICE_ID': JSON.stringify(env.VITE_EMAILJS_SERVICE_ID),
+      'import.meta.env.VITE_EMAILJS_TEMPLATE_ID': JSON.stringify(env.VITE_EMAILJS_TEMPLATE_ID),
+      'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
+      'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY),
+      'import.meta.env.VITE_GOOGLE_CLIENT_ID': JSON.stringify(env.VITE_GOOGLE_CLIENT_ID),
+    },
+  };
 });
